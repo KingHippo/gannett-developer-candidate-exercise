03f3ec018c2b049c9b4ac39a63e2a76b596d1ded
KingHippo <askdonno@gmail.com>
2016-08-01 02:57:28 -0400

    Remove unecessary variables. Comment code for clarity

--- get_json.js
+++ get_json.js
@@ -3,17 +3,17 @@
 
   app.controller('UserController', function($scope, $q, $http) {
     var vm = this;
+    // holds link for JSON: articles
     $scope.articles = '';
+    // holds value for css theme
     $scope.theme = '';
+    // link to atricles - requires profileId
     var content = 'https://crossorigin.me/https://peaceful-springs-7920.herokuapp.com/content/';
-
-    // link to profile id
+    // link to profileId
     var profile = 'https://crossorigin.me/https://peaceful-springs-7920.herokuapp.com/profile/';
-    var profileId = '';
-    var themeId = '';
-
+    // call function to create link for articles
     getId(profile);
-
+    // function to get articles from var profile
     function getArticles() {
 
       $http({
@@ -22,17 +22,21 @@
         })
         .then(function(results) {
           // console.log(results);
+          // fetches data from artices array
           $scope.articles = results.data.articles;
+          // fetches theme for css
           $scope.theme = results.data.theme;
           console.log($scope.theme);
         })
         .catch(function(results) {
           // console.log('getArticles',results);
+          console.error("Error: "results);
         });
     }
-
+    // function to get profileId number
     function getId(data) {
       // console.log(data);
+      // wait for data to be retrieved from getArticles
       var deferred = $q.defer();
 
       $http({
@@ -41,7 +45,9 @@
         })
         .then(function(results) {
           // console.log(results);
+          // concatinate profileId number to var content
           content += results.data.profileId;
+          // callback for getArticles called once var content is updated
           getArticles();
           deferred.resolve();
         })
